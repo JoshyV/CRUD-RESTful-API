9e7b19a1360a4329a66c26ddb1c9fb228ab07d57
Josh Baldespinosa <54145281+JoshyV@users.noreply.github.com>
2024-04-20 17:32:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const jwt = require('jsonwebtoken');
 const port = 3000;
 const secretKey = process.env.ACCESS_TOKEN;
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -21,12 +18,8 @@ const connection = mysql.createPool({
     database: process.env.MYSQL_DATABASE,
 });
 
-app.listen(port, () => {
-  console.log(`Cart2Go Listening to https://c2g.dev:${port}`);
-});
-
 app.get("/", (req, res) => {
-  res.json({ message: "C2G" });
+  res.json({ message: "C2G V1.2" });
 });
 
 function verifyToken(req, res, next) {
@@ -37,29 +30,78 @@ function verifyToken(req, res, next) {
     next();
   }else return res.sendStatus(401);
 }
-//Send the websocket
-function sendDataToClients() {
-  connection.query('SELECT *  FROM products;', (error, results, fields) => {
-    if (error) throw error;
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(results));
-      }
-    });
-  });
-}
-//Websocket
-wss.on('connection', function connection(ws) {
-    console.log('New client connected');
-  ws.on('close', function close() {
-    console.log('Client disconnected');
-  });
-  wss.send("Connected");
-  // Send data to newly connected client
-  sendDataToClients();
+
+app.get("/getUpdates",verifyToken, async (req, res) => {
+  try {
+      const data = await connection.promise().query(
+          `SELECT * FROM products`
+      );
+      res.status(200).json(data[0]);
+  } catch (err) {
+      console.error("Error fetching updates:", err);
+      res.status(500).json({
+          message: "Error fetching updates",
+          error: err
+      });
+  }
 });
 
-//Show all products
+app.post("/login", verifyToken, async (req, res) => {
+  const { username, password } = req.body;
+  if (!username||password) {
+    return res.status(400).json({ message: "Username and password is required in the query parameter." });
+  }
+  try{
+      const data = await connection.promise().query(
+        `SELECT * FROM users WHERE username = ?`,
+        [username],
+        (err, results) => {
+          if (err) {
+            return res.status(500).json({ message: "Internal server error" });
+          }
+          if (results.length === 0) {
+            return res.status(400).json({ message: "User not found" });
+          }
+          const user = results[0];
+          if (user.password !== password) {
+            return res.status(400).json({ message: "Wrong password" });
+          }
+          res.json({ message: "Login successful" });
+        }
+    );
+    res.status(200).json(data[0]);
+  } catch(err){
+    console.error("Error fetching user: ", err);
+    res.status(500).json({
+        message: "Error fetching user!",
+        error: err
+    });
+  }
+});
+
+app.get("/search", verifyToken, async (req, res) => {
+  const brandName = req.query.brand;
+
+  if (!brandName) {
+      return res.status(400).json({ message: "Brand name is required in the query parameter." });
+  }
+
+  try {
+      const data = await connection.promise().query(
+          `SELECT * FROM products WHERE brand = ?`,
+          [brandName]
+      );
+
+      res.status(200).json(data[0]);
+  } catch (err) {
+      console.error("Error fetching products:", err);
+      res.status(500).json({
+          message: "Error fetching products",
+          error: err
+      });
+  }
+});
+
 app.get("/products", verifyToken,async(req, res) => {
     try {
         const data = await connection.promise().query(
@@ -75,7 +117,6 @@ app.get("/products", verifyToken,async(req, res) => {
       }
 });
 
-//Update by ID
 app.put('/product/:id',verifyToken, async (req, res) => {
   const id = req.params.id;
   const { barcode, brand, variant, volume, description } = req.body;
@@ -106,7 +147,6 @@ app.put('/product/:id',verifyToken, async (req, res) => {
   }
 });
 
-//Insert Product
 app.post("/product",verifyToken, async (req, res) => {
   const { barcode, brand, variant,volume,description } = req.body;
 
@@ -136,4 +176,8 @@ app.post("/product",verifyToken, async (req, res) => {
       message: err,
     });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Cart2Go Listening to https://c2g.dev:${port}`);
 });
